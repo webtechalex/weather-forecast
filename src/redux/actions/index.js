d22ae215e609b6e57b0b860aeb05288507680ef5
Alex Brown <alexanderbrown.info@gmail.com>
2018-10-25 11:31:59 +0100

    test error action

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,3 +1,6 @@
 export const FETCH_FORECAST_LOADING = 'FETCH_FORECAST_LOADING'
+export const FETCH_FORECAST_ERROR = 'FETCH_FORECAST_ERROR'
 
 export const fetchForecastLoading = (isLoading) => ({ type: FETCH_FORECAST_LOADING, payload: isLoading || false })
+
+export const fetchForecastError = (hasError) => ({ type: FETCH_FORECAST_ERROR, payload: hasError || false })
--- src/redux/actions/index.spec.js
+++ src/redux/actions/index.spec.js
@@ -1,5 +1,5 @@
-import { FETCH_FORECAST_LOADING } from './'
-import { fetchForecastLoading } from './'
+import { FETCH_FORECAST_LOADING, FETCH_FORECAST_ERROR } from './'
+import { fetchForecastLoading, fetchForecastError } from './'
 
 describe('fetchForecastLoading', () => {
   describe('given no argument', () => {
@@ -20,6 +20,30 @@ describe('fetchForecastLoading', () => {
     it('returns a FETCH_FORECAST_LOADING action with payload of boolean "false"', () => {
       const action = fetchForecastLoading(false)
       expect(action.type).toBe(FETCH_FORECAST_LOADING)
+      expect(action.payload).toBe(false)
+    })
+  })
+})
+
+describe('fetchForecastError', () => {
+  describe('given no argument', () => {
+    it('returns a FETCH_FORECAST_ERROR action with payload of boolean "false"', () => {
+      const action = fetchForecastError()
+      expect(action.type).toBe(FETCH_FORECAST_ERROR)
+      expect(action.payload).toBe(false)
+    })
+  })
+  describe('given a boolean "true" argument', () => {
+    it('returns a FETCH_FORECAST_ERROR action with payload of boolean "true"', () => {
+      const action = fetchForecastError(true)
+      expect(action.type).toBe(FETCH_FORECAST_ERROR)
+      expect(action.payload).toBe(true)
+    })
+  })
+  describe('given a boolean "false" argument', () => {
+    it('returns a FETCH_FORECAST_ERROR action with payload of boolean "false"', () => {
+      const action = fetchForecastError(false)
+      expect(action.type).toBe(FETCH_FORECAST_ERROR)
       expect(action.payload).toBe(false)
     })
   })
